662aafe99e4126fe57f6777b4a04de6153e1c545
y-mabuchi <mabu.you@gmail.com>
2021-05-26 14:51:47 +0900

    add filter

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/y-mabuchi/go-zipcode-v1/csv"
 )
@@ -15,14 +16,24 @@ func init() {
 
 func main() {
 	filename := flag.String("filename", "zipcode.csv", "CSV file name of Zipcode.")
+	word := flag.String("filterword", "", "Filter CSV data by this word.")
 	flag.Parse()
+
 	fmt.Printf("file name: %v\n", *filename)
+	fmt.Printf("filter word: %v\n", *word)
 
 	c := csv.NewCsv(*filename)
 	fmt.Printf("filename in Csv struct: %v\n", c.GetFilename())
 
 	data := c.Read()
+	var results [][]string
 	for _, line := range data {
-		fmt.Println(line)
+		if strings.Contains(line[8], *word) || strings.Contains(line[9], *word) {
+			results = append(results, line)
+		}
+	}
+
+	for _, r := range results {
+		fmt.Println(r)
 	}
 }
